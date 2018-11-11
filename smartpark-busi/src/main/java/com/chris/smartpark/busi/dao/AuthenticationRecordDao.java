984ecbc31f7df71dd38df9eb376b3b0dee5c2533
youguess666 <643723180@qq.com>
2018-11-11 16:54:14 +0800

    预约审核基础类

---
+++ smartpark-busi/src/main/java/com/chris/smartpark/busi/dao/AuthenticationRecordDao.java
@@ -0,0 +1,17 @@
+package com.chris.smartpark.busi.dao;
+
+import com.chris.smartpark.busi.entity.AuthenticationRecordEntity;
+import com.chris.base.modules.sys.dao.BaseDao;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 预约审核表
+ * 
+ * @author chris
+ * @email 258321511@qq.com
+ * @since Nov 09.18
+ */
+@Mapper
+public interface AuthenticationRecordDao extends BaseDao<AuthenticationRecordEntity> {
+	
+}
